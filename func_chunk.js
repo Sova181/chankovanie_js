b6013bed44f8397ccc58bd0953fa615038ea7022
Таня Дашипилова <tanya.volk01@mail.ru>
2024-05-15 21:03:00 +0800

    код работает правильно

---
+++ func_chunk.js
@@ -0,0 +1,14 @@
+function chunk(array, chunkSize) {
+    //Пустой массив, в который будем добавлять куски
+    let result = [];
+  
+    for (let i = 0; i < array.length; i += chunkSize) {
+      // Вырезаем кусок массива заданного размера и добавляем его в полученный массив
+      result.push(array.slice(i, i + chunkSize));
+    }
+
+    return result;
+  }
+  
+  console.log(chunk(['a', 'b', 'c', 'd'], 2));
+  // [['a', 'b'], ['c', 'd']]
